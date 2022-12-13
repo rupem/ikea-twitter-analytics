0979f0ced077c5da349cace6d7f31e8228cf4bd0
rupem <rupesh.mokshagundam@gmail.com>
2022-12-13 13:18:20 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
-setup(name='IKEA Analytics',
+setup(name='ikea Analytics',
       version='1.0',
       description='IKEA Analytics',
       author='Rupesh M',
       author_email='rupesh.mokshagundam@gmail.com',
       url='https://github.com/rupem/ikea-twitter-analytics',
       packages=['ikea_analytics'],
+      install_requires=[
+        requests
+    ],
      )
