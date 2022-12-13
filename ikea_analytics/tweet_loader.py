44bf0f831ef830cf69a5d84a606f1c1942a6d06c
rupem <rupesh.mokshagundam@gmail.com>
2022-12-13 15:36:10 +0530

    batch read from twitter

---
+++ ikea_analytics/tweet_loader.py
@@ -0,0 +1,18 @@
+import tweepy
+# import pyyaml module
+import yaml
+from yaml.loader import SafeLoader
+
+# Open the file and load the file
+with open('User_details.yaml') as f:
+    user_data = yaml.load(f, Loader=SafeLoader)
+    
+
+
+client = tweepy.Client(bearer_token=user_data['bearer_token'])
+
+query = '#IKEA -is:retweet lang:en'
+tweets = client.search_recent_tweets(query=query, tweet_fields=['id,text,created_at,public_metrics'], max_results=100)
+
+for t in tweets:
+    print(t.)
