e1c5946e79124ddbfa410804be5fe1edd6722448
rupem <rupesh.mokshagundam@gmail.com>
2022-12-15 11:07:34 +0530

    insights

--- ikea_analytics/batch_processing/tweet_analyzer.py
+++ ikea_analytics/batch_processing/tweet_analyzer.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import logging
 
+import psycopg2
+
+import pandas as pds
+
+from sqlalchemy import create_engine,text
+
+# Create an engine instance
+
+alchemyEngine   = create_engine('postgresql+psycopg2://postgres:moksha93@127.0.0.1:5432/ikea_analytics', pool_recycle=3600);
+
 def analyze_tweets():
     logging.basicConfig(level=logging.INFO)
-    logging.info("********** Loading tweets analysis **************")
-    
-
+    logging.info("********** Loading tweets for analysis **************")
+    sql = '''
+    SELECT * FROM tweets;
+    '''
+    with alchemyEngine.connect().execution_options(autocommit=True) as conn:
+        query = conn.execute(text(sql))         
+    df = pd.DataFrame(query.fetchall())
+    print(df)
     logging.info("********** Loading processed tweets **************")
     
-
-def get_tweet_sentiment(self, tweet):
-        '''
-        Utility function to classify sentiment of passed tweet
-        using textblob's sentiment method
-        '''
-        # create TextBlob object of passed tweet text
-        analysis = TextBlob(self.clean_tweet(tweet))
-        # set sentiment
-        if analysis.sentiment.polarity > 0:
-            return 'positive'
-        elif analysis.sentiment.polarity == 0:
-            return 'neutral'
-        else:
-            return 'negative'
+analyze_tweets()
